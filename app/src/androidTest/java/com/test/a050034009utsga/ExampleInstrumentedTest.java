e23d9251866f243b5d74dae59e4e08b2c2bbcec7
NaufalStrict0 <1911500034@mahasiswa.atmaluhur.ac.id>
2022-04-21 00:42:03 +0700

    update form lokasi kebakaran

--- app/src/androidTest/java/com/test/a050034009utsga/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/test/a050034009utsga/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.latihan.a050_034_009_uts_ga;
+package com.test.a050034009utsga;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.latihan.a050_034_009_uts_ga", appContext.getPackageName());
+        assertEquals("com.test.a050034009utsga", appContext.getPackageName());
     }
 }
